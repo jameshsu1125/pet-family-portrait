dadb64db22cbe833b42f2a4bc2a8899f92e36360
jameshsu1125 <jameshsu1125@gmail.com>
2024-03-06 23:58:31 +0800

    Delete unused files and update Tailwind CSS configuration

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,19 +1,13 @@
 import { memo, useState } from 'react';
 import { HomeContext, HomeState, THomeState } from './config';
 import './index.less';
-import { SETTING } from '../../../setting';
-import Table from './table';
 
 const Home = memo(() => {
   const [state, setState] = useState<THomeState>(HomeState);
 
   return (
     <div className='Home'>
-      <HomeContext.Provider value={[state, setState]}>
-        {SETTING.mongodb.map((data) => (
-          <Table key={data.collection} data={[data]} />
-        ))}
-      </HomeContext.Provider>
+      <HomeContext.Provider value={[state, setState]}>asd</HomeContext.Provider>
     </div>
   );
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,56 +1,30 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
+import color from 'tailwindcss/colors';
+import { fontSize } from 'tailwindcss/defaultTheme';
+
+delete color.lightBlue;
+delete color.warmGray;
+delete color.trueGray;
+delete color.coolGray;
+delete color.blueGray;
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  daisyui: {
-    themes: [
-      {
-        currentTheme: {
-          primary: '#00bdff',
-          secondary: '#009400',
-          accent: '#00c0ff',
-          neutral: '#2c1a19',
-          'base-100': '#2b2b2b',
-          info: '#00cdff',
-          success: '#a9d426',
-          warning: '#cd8700',
-          error: '#ff3d69',
-        },
-      },
-      'light',
-      'dark',
-      'cupcake',
-      'bumblebee',
-      'emerald',
-      'corporate',
-      'synthwave',
-      'retro',
-      'cyberpunk',
-      'valentine',
-      'halloween',
-      'garden',
-      'forest',
-      'aqua',
-      'lofi',
-      'pastel',
-      'fantasy',
-      'wireframe',
-      'black',
-      'luxury',
-      'dracula',
-      'cmyk',
-      'autumn',
-      'business',
-      'acid',
-      'lemonade',
-      'night',
-      'coffee',
-      'winter',
-      'dim',
-      'nord',
-      'sunset',
-    ],
+  theme: {
+    fontSize: {
+      ...fontSize,
+    },
+    colors: {
+      ...color,
+      primary: '#870000',
+      secondary: '#ff6600',
+      tertiary: '#ff0066',
+      quaternary: '#00ff00',
+      backgroundColor: '#111',
+      textColor: '#eee',
+    },
+    extend: {},
   },
-  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  plugins: ['prettier-plugin-tailwindcss'],
 };
